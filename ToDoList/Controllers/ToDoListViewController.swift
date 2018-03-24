ab29da3358fcfdcf22ff7b77549132aef96d9d86
AkkiSuju <akki.suju@gmail.com>
2018-03-24 22:52:05 +0530

    Data persistence with Codable

--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -17,39 +17,49 @@ class ToDoListViewController: UITableViewController
     var itemArray = [Item]()
     
     
-    // Creating a variable of UserDefaults type
+    /* Here we are creating a file path to
+     Documents folder. To understand it better
+     see the video of Sandboxing (Section 19,
+     Lecture 245)
+     
+     File Manager provides a convenient
+     interface to the contents of the file
+     system.
+     
+     Here we are tapping into Document Directory,
+     and we are looking for it inside User Domain
+     Mask i.e. user's home directory, that place
+     where we are saving their personal items
+     associated with this app.
+     
+     The urls method returns an array of URLs for
+     the specified directory, so, we are grabbing
+     its first item.
+     
+     We are adding a component i.e. a file to this
+     path, which is a plist file in this case.
+     And we will use this file to save user data
+     persistently. */
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Item One"
-        itemArray.append(newItem)
         
-        let newItem2 = Item()
-        newItem2.title = "Item Two"
-        itemArray.append(newItem2)
+        /* This is to print the path of the Items.plist
+         file, just to get the idea that where it gets
+         saved. */
         
-        let newItem3 = Item()
-        newItem3.title = "Item Three"
-        itemArray.append(newItem3)
+        print(dataFilePath)
         
-        /* Here we are saving the value from the
-         User Defaults to the itemArray.  Also, we
-         are doing Optional Binding just to check
-         if the ToDoListArray exists.
-         
-         We are casting User Defaults as an array of
-         Strings*/
+        /* call to the loadItems method to retrieve
+         the data from the plist file. */
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item]
-        {
-            itemArray = items
-        }
+        loadItems()
+        
     }
     
     
@@ -58,15 +68,17 @@ class ToDoListViewController: UITableViewController
     
     /********************************/
     
-    //MARK: - Table View Datasource Methods
+    //MARK: - Table View DataSource Methods
     
     
     /* Refer to WhatsApp coding to get explanation
-      for the below two methods. */
+     for the below two methods. */
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return itemArray.count
     }
+    
     
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
@@ -89,7 +101,7 @@ class ToDoListViewController: UITableViewController
         cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
-
+        
     }
     
     
@@ -101,11 +113,15 @@ class ToDoListViewController: UITableViewController
     
     
     /* Refer to WhatsApp coding to get explanation
-     for the below two methods. */
+     for the below two methods.
+     
+     A little bit of explanation is as such, when
+     user will click a row then this method will
+     execute, and will put or remove checkmark on
+     that row. */
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        //print(itemArray[indexPath.row])
         
         /* Here we are using not operator (!).
          This will set the DONE property (we have
@@ -113,11 +129,27 @@ class ToDoListViewController: UITableViewController
          current item just to opposite of what it is
          already set.  It means, if DONE property is
          TRUE then it will set to FALSE, and vice-versa.
-        */
+         
+         Then with the help of this property we can
+         check and uncheck the checkmark on a cell/row
+         inside cellForRowAtIndexPath method above.
+         */
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
+        
+        /* The above statement will set the DONE
+         property, and accordingly checkmark wil be
+         visible or invisible on a row in Table View.
+         
+         But we have to save that status of DONE property
+         in the plist file too, therefore, here we are
+         calling the saveItems method to save the
+         DONE property in the plist file. */
+        
+        saveItems()
+        
+        
         
         /* By default, on selecting a row, it gets
          permanently selected until we click on
@@ -153,6 +185,7 @@ class ToDoListViewController: UITableViewController
         
         let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
         
+        
         let action = UIAlertAction(title: "Add Item", style: .default) { (UIAlertAction) in
             
             
@@ -163,21 +196,11 @@ class ToDoListViewController: UITableViewController
             self.itemArray.append(newItem)
             
             
-            /* Here we are adding the updated array to the
-             User Default. The below statement provides many
-             options once we start typing set.  We have
-             to chose this option -
-             set(value: Any?, forKey: String)
-             
-             forKey would be a name that will identify the array
-             inside the User Default. */
+            /* Calling the saveItems method to save the
+             itemArray to the plist file. */
             
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            self.saveItems()
             
-            /* after adding new item, we are reloading the
-             table to see the new item. */
-            
-            self.tableView.reloadData()
         }
         
         
@@ -191,8 +214,7 @@ class ToDoListViewController: UITableViewController
          in this case).  Delete the code placeholder and give
          your own code. */
         
-        alert.addTextField { (alertTextField) in
-            alertTextField.placeholder = "Create new item"
+        alert.addTextField { (alertTextField) in alertTextField.placeholder = "Create new item"
             
             
             /* Because the alertTextField has a scope just
@@ -206,9 +228,79 @@ class ToDoListViewController: UITableViewController
         }
         
         alert.addAction(action)
+        
         present(alert, animated: true, completion: nil)
     }
     
-
+    
+    
+    
+    /********************************/
+    
+    //MARK: - Model Manupulation Method
+    
+    
+    /* Defining a use defined method that will save the
+     itemArray to the plist file. */
+    
+    func saveItems()
+    {
+        /* Here we creating an object of
+         PropertyListEncoder, which encodes instances of
+         data types from a property list. */
+        
+        let encoder = PropertyListEncoder()
+        
+        
+        /* Here we are encoding the itemArray and then
+         writing it to the data file path.  Because both
+         of the statements could throw exception,
+         therefore, have enclosed them in do-catch block.
+         */
+        
+        do
+        {
+            let data = try encoder.encode(itemArray)
+            
+            try data.write(to: dataFilePath!)
+        }
+        catch
+        {
+            print("Error encoding item array, \(error)")
+        }
+        
+        /* after adding new item, we are reloading the
+         table to see the new item. */
+        
+        tableView.reloadData()
+        
+    }
+    
+    
+    /* This is the user-defined method to decode and
+     retrieve the data from the plist file.
+     */
+    
+    func loadItems()
+    {
+        /* contentsOf method can throw an exception,
+         therefore, we have done optional binding with
+         it.  And the reason of optional binding instead
+         of do-catch block is bcz we don't have to treat
+         with the error msg. */
+        
+        if let data = try? Data(contentsOf: dataFilePath!)
+        {
+            let decoder = PropertyListDecoder()
+            do
+            {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch
+            {
+                print("Error decoding item array, \(error)")
+            }
+        }
+    }
+    
 }
-
--- ToDoList/Data Model/Item.swift
+++ ToDoList/Data Model/Item.swift
@@ -10,10 +10,20 @@ import Foundation
 
 
 /* We will use this class as a blueprint to
- create items for our To Do List. */
+ create items for our To Do List.
+ 
+ We are also conforming to the protocol
+ Codable.  It means that the item type is
+ now enable to encode as well as decode itself
+ to an external representations, such as -
+ plist, JSON etc.
+ 
+ To be able to a Codable, a class must
+ have only standard data types. */
 
-class Item
+class Item: Codable
 {
     var title: String = ""
     var done: Bool = false
 }
+
